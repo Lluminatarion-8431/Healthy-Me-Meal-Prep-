d9299e8fb313c759442d52bbc31732b738677657
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-10-05 11:39:13 -0500

    Models for customer and admin

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,17 @@ namespace Healthy_Me.Models
 {
     public class Admin
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,45 @@ namespace Healthy_Me.Models
 {
     public class Customer
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Display(Name = "Goal")]
+        public string goal { get; set; }
+
+        [Display(Name = "Age")]
+        public double age { get; set; }
+
+        [Display(Name = "Height")]
+        public double height { get; set; }
+
+        [Display(Name = "Weight")]
+        public double weight { get; set; }
+
+        [Display(Name = "Street Name")]
+        public string streetName { get; set; }
+
+        [Display(Name = "House Number")]
+        public int houseNumber { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public int zipCode { get; set; }
+
+        [Display(Name = "City")]
+        public string city { get; set; }
+
+        [Display(Name = "State")]
+        public string state { get; set; }
+
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
 }
