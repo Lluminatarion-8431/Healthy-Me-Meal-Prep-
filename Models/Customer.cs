d888192bcaf4101cd968799ce90fcd07df4c2501
Kevin Harvey Jr <lordty@gmail.com>
2020-10-06 12:59:09 -0500

    Added_Upload_Image

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 
@@ -46,6 +47,8 @@ namespace Healthy_Me.Models
 
         [Display(Name = "State")]
         public string state { get; set; }
+        
+       
 
         
 
