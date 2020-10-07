70d276cc718fa778d24d5994264257707881af41
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-10-07 12:13:37 -0700

    Added HttpClient services in Program class.

--- Startup.cs
+++ Startup.cs
@@ -46,6 +46,7 @@ namespace Healthy_Me
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
