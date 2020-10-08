f405da5d1852d5ab140367cbc37c664790995c0f
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-10-08 08:32:18 -0700

    Created new model for BMI to implement the BMI API.

---
+++ Models/BMI.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Healthy_Me.Models
+{
+    public class BMI
+    {
+
+        public class Rootobject
+        {
+            public Weight weight { get; set; }
+            public Height height { get; set; }
+            public Bmi bmi { get; set; }
+            public string ideal_weight { get; set; }
+            public string surface_area { get; set; }
+            public string ponderal_index { get; set; }
+            public Bmr bmr { get; set; }
+            public Whr whr { get; set; }
+            public Whtr whtr { get; set; }
+        }
+
+        public class Weight
+        {
+            public string kg { get; set; }
+            public string lb { get; set; }
+        }
+
+        public class Height
+        {
+            public string m { get; set; }
+            public string cm { get; set; }
+            public string _in { get; set; }
+            public string ftin { get; set; }
+        }
+
+        public class Bmi
+        {
+            public string value { get; set; }
+            public string status { get; set; }
+            public string risk { get; set; }
+            public string prime { get; set; }
+        }
+
+        public class Bmr
+        {
+            public string value { get; set; }
+        }
+
+        public class Whr
+        {
+            public string value { get; set; }
+            public string status { get; set; }
+        }
+
+        public class Whtr
+        {
+            public string value { get; set; }
+            public string status { get; set; }
+        }
+
+    }
+}
